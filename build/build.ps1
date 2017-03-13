df1a4881133065d3e1f09122d54f31b61248e0ca
Tomas Matousek <tomat@microsoft.com>
2017-03-13 11:39:42 -0700

    Switch to RepoToolset infrastructure

---
+++ build/build.ps1
@@ -0,0 +1,79 @@
+[CmdletBinding(PositionalBinding=$false)]
+Param(
+  [string] $configuration = "Debug",
+  [string] $solution = "",
+  [string] $verbosity = "normal",
+  [switch] $restore,
+  [switch] $build,
+  [switch] $test,
+  [switch] $sign,
+  [switch] $pack,
+  [switch] $ci
+)
+
+set-strictmode -version 2.0
+$ErrorActionPreference = "Stop"
+
+$RepoRoot = Join-Path $PSScriptRoot "..\"
+$ToolsRoot = Join-Path $RepoRoot ".tools"
+$BuildProj = Join-Path $PSScriptRoot "build.proj"
+$DependenciesProps = Join-Path $PSScriptRoot "Versions.props"
+$ArtifactsDir = Join-Path $RepoRoot "artifacts"
+$LogDir = Join-Path $ArtifactsDir "log"
+$TempDir = Join-Path (Join-Path $ArtifactsDir $configuration) "tmp"
+
+function Create-Directory([string[]] $path) {
+  if (!(Test-Path -path $path)) {
+    New-Item -path $path -force -itemType "Directory" | Out-Null
+  }
+}
+
+function GetVSWhereVersion {
+  [xml]$xml = Get-Content $DependenciesProps
+  return $xml.Project.PropertyGroup.VSWhereVersion
+}
+
+function LocateMsbuild {
+  
+  $vswhereVersion = GetVSWhereVersion
+  $vsWhereDir = Join-Path $ToolsRoot "vswhere\$vswhereVersion"
+  $vsWhereExe = Join-Path $vsWhereDir "vswhere.exe"
+    
+  if (!(Test-Path $vsWhereExe)) {
+    Create-Directory $vsWhereDir   
+    Invoke-WebRequest "http://github.com/Microsoft/vswhere/releases/download/$vswhereVersion/vswhere.exe" -OutFile $vswhereExe
+  }
+  
+  $vsInstallDir = & $vsWhereExe -property installationPath -requires Microsoft.Component.MSBuild -requires Microsoft.VisualStudio.Component.VSSDK -requires Microsoft.Net.Component.4.6.TargetingPack -requires Microsoft.VisualStudio.Component.Roslyn.Compiler -requires Microsoft.VisualStudio.Component.VSSDK
+  $msbuildExe = Join-Path $vsInstallDir "MSBuild\15.0\Bin\msbuild.exe"
+
+  if (!(Test-Path $msbuildExe)) {
+    throw "Failed to locate msbuild (exit code '$lastExitCode')."
+  }
+
+  return $msbuildExe
+}
+
+function Build {
+  $msbuildExe = LocateMsbuild
+  
+  $summaryLog = Join-Path $LogDir "Build.log"
+  $warningLog = Join-Path $LogDir "Build.wrn"
+  $errorLog = Join-Path $LogDir "Build.err"
+
+  Create-Directory($logDir)
+  
+  & $msbuildExe $BuildProj /p:Configuration=$configuration /p:SolutionPath=$solution /p:Restore=$restore /p:Build=$build /p:Test=$test /p:Sign=$sign /p:Pack=$pack /p:CIBuild=$ci /v:$verbosity /flp1:Summary`;Verbosity=diagnostic`;Encoding=UTF-8`;LogFile=$summaryLog /flp2:WarningsOnly`;Verbosity=diagnostic`;Encoding=UTF-8`;LogFile=$warningLog /flp3:ErrorsOnly`;Verbosity=diagnostic`;Encoding=UTF-8`;LogFile=$errorLog
+
+  if ($lastExitCode -ne 0) {
+    throw "Build failed (exit code '$lastExitCode')."
+  }
+}
+
+if ($ci) {
+  Create-Directory $TempDir
+  $env:TEMP = $TempDir
+  $env:TMP = $TempDir
+}
+
+Build
