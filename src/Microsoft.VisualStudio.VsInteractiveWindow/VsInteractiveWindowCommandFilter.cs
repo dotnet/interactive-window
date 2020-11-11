bdf23a0e2688e389b05c8816215ac4c739289047
Tomas Matousek <tomat@microsoft.com>
2020-11-11 13:02:59 -0500

    Use Microsoft.NET.Sdk.WindowsDesktop

--- src/Microsoft.VisualStudio.VsInteractiveWindow/VsInteractiveWindowCommandFilter.cs
+++ src/Microsoft.VisualStudio.VsInteractiveWindow/VsInteractiveWindowCommandFilter.cs
@@ -5,6 +5,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Windows.Input;
 using Microsoft.VisualStudio.Editor;
 using Microsoft.VisualStudio.OLE.Interop;
 using Microsoft.VisualStudio.Shell;
